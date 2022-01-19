cc29f299141b1c54f8b1708527e693ef0ef9f1de
WON-JIN-LEE <vopgh0409@gmail.com>
2022-01-19 22:34:02 +0900

    wonjin

--- wonjin/test.js
+++ wonjin/test.js
@@ -1,9 +1,8 @@
-let nums = [6, 2, 6, 5, 1, 2];
+const input = `6
+(())())
+(((()())()
+(()())((()))
+((()()(()))(((())))()
+()()()()(()()())()
+(()((())()(`.split("\n");
 
-var arrayPairSum = function (nums) {
-  nums.sort((a, b) => a - b);
-  const answer = nums.filter((_, i) => i % 2 === 0);
-  return answer.reduce((sum, cur) => sum + cur, 0);
-};
-
-console.log(arrayPairSum(nums));
--- wonjin/test.py
+++ wonjin/test.py
@@ -1,36 +1,44 @@
-from typing import List, Optional
+import sys
+input = sys.stdin.readline
+from collections import deque
 
 
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+def get_card(num):
+    # 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
 
+    # 1 ~ N번까지 카드를 데크로 생성
+    queue = deque([n for n in range(1, num+1)])
+    print('1 ~ N번까지 카드 : ', queue) 
 
-class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
+    i=1
+    print("==========================")
 
-        odd = head
-        even = head.next
-        even_head = head.next
+    # 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다.
+    while len(queue) > 1:
+        print(f"반복횟수: {i}")
+        i += 1
 
-        while even and even.next:
-            odd.next = odd.next.next
-            even.next = even.next.next
-            odd = odd.next
-            even = even.next
-            print(odd)
-            print(even)
-
-        odd.next = even_head
-        return head
+        # 제일 위에 있는 카드를 바닥에 버린다. 제거한다.
+        print('제일 위에 있는 카드 제거 전 : ', queue) 
+        queue.popleft()
+        print('제일 위에 있는 카드 제거 후 : ', queue) 
+        
+        #  제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+        queue.append(queue.popleft())
+        print('위에 있는 카드 -> 아래로 옮긴다. : ', queue) 
+        print("queue len:", len(queue))
+        print("==========================")
+    return queue.pop()
 
 
 if __name__ == '__main__':
-    head = ListNode(1, ListNode(2, ListNode(
-        3, ListNode(4, ListNode(5, None)))))
-    LinkedFun = Solution()
-    LinkedFun.oddEvenList(head)
+
+    # 입력값 받아오기
+    # n = int(input())
+    # print(get_card(n))
+    
+    queue = get_card(6)
+    print("answer:",queue)
+
+    
+    
